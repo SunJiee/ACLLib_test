7e3d2d118130df9bccd518272be9ec0834462e87
SunJiee <760941378@qq.com>
2020-05-14 16:47:14 +0800

    no message

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include "acllib.h"
+#include <stdio.h>
 
 int Setup() {
-  initWindow("test", 100, 100, 200, 200);
+  initWindow("Mywin", DEFAULT, DEFAULT, 600, 600);
+  beginPaint();
+  setPenColor(BLUE);
+  setPenWidth(1);
+  setPenStyle(PEN_STYLE_DASHDOTDOT);
+  line(20, 20, 600-20, 600-20);
+  putPixel(100,150,GREEN);
+  endPaint();
   return 0;
 }
